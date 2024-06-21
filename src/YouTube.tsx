64bd611b0de34c6022d1f106d454aa786b7c36f8
Kittyhaus <meow@kittyhaus.tv>
2024-06-20 22:24:24 -0700

    use react-player for youtube player

--- src/YouTube.tsx
+++ src/YouTube.tsx
@@ -1,21 +1,21 @@
-
+import ReactPlayer from 'react-player/youtube'
 import './YouTube.css'
 
 interface YouTubeProps {
 	embedId: string
 }
 
-const YouTube = (props: YouTubeProps) => {
-	const { embedId } = props
-
+const YouTube = ({ embedId }: YouTubeProps) => {
 	return (
-		<iframe
-			className='youtube-iframe'
-			src={`https://www.youtube.com/embed/${embedId}?mute=1&autoplay=1&controls=0`}
-			allow="accelerometer; autoplay; clipboard-write; encrypted-media; fullscreen; gyroscope; picture-in-picture"
-			title="Embedded youtube"
-		/>
+		<ReactPlayer
+				url={`https://www.youtube.com/watch?v=${embedId}`}
+				volume={1}
+				muted={true}
+				playing={true}
+				controls={false}
+				style={{ width: 'auto', height: 'auto', minHeight: '100vh', minWidth: '100vw', position: 'fixed', pointerEvents: 'none' }}
+			/>
 	)
 }
-  
+
 export default YouTube;
